9f6b02c71f4a695e1092f9962551c919c10a00f9
Gustavo Ribeiro <gustavo.mribeiroxg@yahoo.com>
2023-01-24 17:06:15 -0300

    adding listView code

--- app/src/main/java/com/dev/myappcompany/timestables/MainActivity.java
+++ app/src/main/java/com/dev/myappcompany/timestables/MainActivity.java
@@ -3,6 +3,11 @@ package com.dev.myappcompany.timestables;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SeekBar;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar timesTablesSeekBar = findViewById(R.id.seekBar);
+
+        ListView myListView = findViewById(R.id.myListView);
+
+        final ArrayList<String> companies = new ArrayList<String>();
+
+        companies.add("Google");
+        companies.add("Amazon");
+        companies.add("Microsoft");
+        companies.add("Apple");
+        companies.add("Meta");
+        companies.add("Netflix");
+        companies.add("Spotify");
+        companies.add("Samsung");
+        companies.add("Tesla");
+        companies.add("Space X");
+        companies.add("Loft");
+        companies.add("Nike");
+        companies.add("Adidas");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, companies);
+
+        myListView.setAdapter(arrayAdapter);
+
+
+
     }
 }
