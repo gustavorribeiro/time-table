9361577e546247ed8de6b207ccaab903140e770c
Gustavo Ribeiro <gustavo.mribeiroxg@yahoo.com>
2023-01-24 17:27:50 -0300

    adding seekBar and adjusting layout constraints

--- app/src/main/java/com/dev/myappcompany/timestables/MainActivity.java
+++ app/src/main/java/com/dev/myappcompany/timestables/MainActivity.java
@@ -3,6 +3,7 @@ package com.dev.myappcompany.timestables;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SeekBar;
@@ -16,9 +17,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SeekBar timesTablesSeekBar = findViewById(R.id.seekBar);
+        final SeekBar timesTablesSeekBar = findViewById(R.id.seekBar);
 
         ListView myListView = findViewById(R.id.myListView);
+
+        timesTablesSeekBar.setMax(15);
+        timesTablesSeekBar.setProgress(5);
+
+        timesTablesSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int min = 1;
+                int timesTableNumber;
+
+                if (progress < min) {
+                    timesTableNumber = min;
+                } else {
+                    timesTableNumber = progress;
+                }
+                Log.i("Seekbar value: ", Integer.toString(timesTableNumber));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
         final ArrayList<String> companies = new ArrayList<String>();
 
@@ -35,6 +64,8 @@ public class MainActivity extends AppCompatActivity {
         companies.add("Loft");
         companies.add("Nike");
         companies.add("Adidas");
+        companies.add("Mercedes-Benz");
+        companies.add("BMW");
 
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, companies);
 
